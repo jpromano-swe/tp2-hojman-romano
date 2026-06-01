4bc6762bd1fac6008ab83be4fc2138470050a761
Juan <juanpiromanok@gmail.com>
2026-06-01 10:23:58 -0300

    Implementacion de funciones para crear las estructuras, pendiente de ajustar ABB para que la funcion tenga una unica tarea

---
+++ excel.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+  "encoding/csv"
+  "io"
+  "os"
+  "strconv"
+  "tdas/tdas/diccionario"
+)
+
+func abrirArchivoCSV(archivo string) *os.File {
+  datos, err := os.Open(archivo)
+  if err != nil {
+    panic("Error al leer el archivo")
+  }
+  return datos
+}
+
+func hashearDatosCSV(csvReader *csv.Reader) {
+  pacientes := diccionario.CrearHash[string, int]()
+  for {
+    rec, err := csvReader.Read()
+    if err == io.EOF {
+      break
+    }
+    if err != nil {
+      panic("Error al leer el archivo")
+    }
+    edadInt, err := strconv.Atoi(rec[1])
+    if err != nil {
+      panic("El valor de la columna no es un número válido: " + rec[1])
+    }
+    pacientes.Guardar(rec[0], edadInt)
+  }
+}
+
+// a la q llamamos es a esta
+func obtenerDatosCSV(archivo string) {
+  datos := abrirArchivoCSV(archivo)
+  defer datos.Close()
+  csvReader := csv.NewReader(datos)
+  hashearDatosCSV(csvReader)
+}
--- hospital/hospital.go
+++ hospital/hospital.go
@@ -1,21 +1,24 @@
 package hospital
 
 import (
+  "bufio"
+  "os"
+  "strings"
   "tdas/tdas/cola"
   "tdas/tdas/cola_prioridad"
   "tdas/tdas/diccionario"
-
 )
 
 type Paciente struct {
   nombre     string
   antiguedad int
-  urgencia string
+  urgencia   string
 }
 
 type Medico struct {
-  nombre       string
-  especialidad *Especialidad
+  nombre             string
+  especialidad       *Especialidad
+  pacientesAtendidos int
 }
 
 type Especialidad struct {
@@ -30,3 +33,58 @@ type Hospital struct {
   medicos        diccionario.DiccionarioOrdenado[string, *Medico]
 }
 
+func crearHospital(archivoDoctores string, archivoPacientes string) *Hospital {
+  especialidades := CrearDiccionarioEspecialidades()
+  medicos := crearArbolMedicos(archivoDoctores, especialidades)
+  pacientes := crearDiccionarioPacientes(archivoPacientes)
+
+  return &Hospital{
+    pacientes:      pacientes,
+    especialidades: especialidades,
+    medicos:        medicos,
+  }
+}
+
+func crearMedico(nombre string, especialidad *Especialidad) *Medico {
+  return &Medico{
+    nombre:             nombre,
+    especialidad:       especialidad,
+    pacientesAtendidos: 0,
+  }
+}
+
+func crearEspecialidad(nombre string) *Especialidad {
+  return &Especialidad{
+    nombre:          nombre,
+    turnosUrgentes:  nil,
+    turnosRegulares: nil,
+  }
+}
+
+func CrearDiccionarioEspecialidades() diccionario.Diccionario[string, *Especialidad] {
+  return diccionario.CrearHash[string, *Especialidad]()
+}
+
+func crearArbolMedicos(archivo string, especialidades diccionario.Diccionario[string, *Especialidad]) diccionario.DiccionarioOrdenado[string, *Medico] {
+  arbol := diccionario.CrearABB[string, *Medico](strings.Compare)
+  aperturaArchivo, _ := os.Open(archivo)
+  defer aperturaArchivo.Close()
+
+  scanner := bufio.NewScanner(aperturaArchivo)
+
+  for scanner.Scan() {
+    linea := scanner.Text()
+    arregloDatos := strings.Split(linea, ",")
+    if especialidades.Pertenece(arregloDatos[1]) {
+      nuevaEspecialidad := especialidades.Obtener(arregloDatos[1])
+      nuevoMedico := crearMedico(arregloDatos[0], nuevaEspecialidad)
+      arbol.Guardar(nuevoMedico.nombre, nuevoMedico)
+    } else {
+      nuevaEspecialidad := crearEspecialidad(arregloDatos[1])
+      especialidades.Guardar(nuevaEspecialidad.nombre, nuevaEspecialidad)
+      nuevoMedico := crearMedico(arregloDatos[0], nuevaEspecialidad)
+      arbol.Guardar(nuevoMedico.nombre, nuevoMedico)
+    }
+  }
+  return arbol
+}
